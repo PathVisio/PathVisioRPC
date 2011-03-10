313004f0555706add57ce54ff292cdfcfac3ce1a
martijn <martijn@55a0e026-e783-40ef-8df5-1463b6136d98>
2011-03-10 15:49:43 +0000

    pv.connector: functions for handling a pathway
    git-svn-id: http://svn.bigcat.unimaas.nl/pvplugins/trunk/pv.connector@450 55a0e026-e783-40ef-8df5-1463b6136d98

---
+++ src/org/pathvisio/xmlrpc/PathwayFunctions.java
@@ -0,0 +1,105 @@
+package org.pathvisio.xmlrpc;
+
+import java.io.File;
+
+import javax.jws.WebMethod;
+
+import org.pathvisio.Engine;
+import org.pathvisio.model.ConverterException;
+import org.pathvisio.model.ObjectType;
+import org.pathvisio.model.Pathway;
+import org.pathvisio.model.PathwayElement;
+import org.pathvisio.model.StaticProperty;
+
+public class PathwayFunctions
+{
+	private static Engine engine;
+	
+	public static void setEngine (Engine value)
+	{
+		engine = value;
+	}
+	
+    @WebMethod
+    public String test() {
+        return "It works!";
+    }
+    
+	@WebMethod
+	public boolean newPathway()
+	{
+		engine.newPathway();
+		return true;
+	}
+	
+	@WebMethod
+	public boolean openPathway (String filename) throws ConverterException
+	{
+		engine.openPathway(new File(filename));
+		return true;
+	}
+	
+	@WebMethod
+	public boolean exportPathway (String filename) throws ConverterException
+	{
+		engine.exportPathway(new File(filename), engine.getActivePathway());
+		return true;
+	}
+	
+	@WebMethod
+	public boolean setFlux (String id, double value)
+	{
+		PathwayElement elt = engine.getActivePathway().getElementById(id);
+		elt.setLineThickness(value);
+		return true;
+	}
+
+	@WebMethod
+	public boolean setProperty (String id, String key, String value)
+	{
+		PathwayElement elt = engine.getActivePathway().getElementById(id);
+
+		StaticProperty prop = StaticProperty.valueOf(key);
+		elt.setStaticProperty(prop, value);
+		return true;
+	}
+
+	@WebMethod
+	public boolean setProperty (String id, String key, double value)
+	{
+		PathwayElement elt = engine.getActivePathway().getElementById(id);
+
+		StaticProperty prop = StaticProperty.valueOf(key);
+		elt.setStaticProperty(prop, value);
+		return true;
+	}
+
+	@WebMethod
+	public String getProperty (String id, String key)
+	{
+		PathwayElement elt = engine.getActivePathway().getElementById(id);
+		StaticProperty prop = StaticProperty.valueOf(key);
+		return "" + elt.getStaticProperty(prop);
+	}
+
+	@WebMethod
+	public String addElement (String type)
+	{
+		ObjectType ot = ObjectType.valueOf(type);
+		PathwayElement elt = PathwayElement.createPathwayElement(ot);
+		Pathway pwy = engine.getActivePathway();
+		pwy.add(elt);
+		elt.setGeneratedGraphId();
+		return elt.getGraphId();
+	}
+	
+	@WebMethod
+	public boolean removeElement (String id)
+	{
+		Pathway pwy = engine.getActivePathway();
+		PathwayElement elt = pwy.getElementById(id);
+		pwy.remove(elt);
+		return true;
+	}
+
+}
