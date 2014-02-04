104d0e112995c5db9c14574fc3f73422cae7640b
Anwesha <pennatula@gmail.com>
2014-02-04 15:52:04 +0100

    added test data and plugin properties

---
+++ testdata/pvscript.R
@@ -0,0 +1,6 @@
+library ("XMLRPC")
+server = "http://localhost:5678"
+xml.rpc(server,"PathVisio.test")
+xml.rpc(server, "PathVisio.importData", "home/anwesha/workspacePathVisio/github/PathVisioRPC/testdata/data.txt", "",3, "/home/anwesha/PathVisio-Data/gene-databases",
+			"home/anwesha/workspacePathVisio/github/PathVisioRPC/testdata/results")
+
