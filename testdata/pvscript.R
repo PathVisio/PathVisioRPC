8771c9bb2edd49fe6ee7c866212febb76f0d8a4e
Anwesha <pennatula@gmail.com>
2014-02-05 12:41:51 +0100

    test script changed

--- testdata/pvscript.R
+++ testdata/pvscript.R
@@ -1,6 +1,5 @@
 library ("XMLRPC")
 server = "http://localhost:5678"
 xml.rpc(server,"PathVisio.test")
-xml.rpc(server, "PathVisio.importData", "home/anwesha/workspacePathVisio/github/PathVisioRPC/testdata/data.txt", "",3, "/home/anwesha/PathVisio-Data/gene-databases",
-			"home/anwesha/workspacePathVisio/github/PathVisioRPC/testdata/results")
+xml.rpc(server, "PathVisio.importData", "/home/anwesha/Desktop/data1.txt", "","3", "/home/anwesha/PathVisio-Data/gene-databases","home/anwesha/workspacePathVisio/github/PathVisioRPC/testdata/results")
 
